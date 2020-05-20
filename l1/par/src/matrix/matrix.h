1a8239ad6e1c52dcfff791e3e9ff77f029376813
huerd <2333486+huerd@users.noreply.github.com>
2020-05-20 00:08:12 -0400

    - started parallel template

--- l1/par/src/matrix/matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-
-int ** allocateMatrix(int rows, int cols);
-void deallocateMatrix(int rows, int ** matrix);
-
-void fillMatrix(int rows, int cols, int initialValue, int ** matrix);
-
-#endif
