3c1b328abfea62ebcd362a0bd4d3e88bd625dd07
huerd <2333486+huerd@users.noreply.github.com>
2020-06-04 16:12:09 -0400

    - excluded a line in run.sh

--- l1/par/run.sh
+++ l1/par/run.sh
@@ -2,11 +2,11 @@ make all
 
 echo ''
 echo '>>> 1 0 1'
-mpirun -np 16 ./lab1 1 0 1
+# mpirun -np 16 ./lab1 1 0 1
 echo '<<<'
 
 echo '>>> 1 5 3'
-# mpirun -np 16 ./lab1 1 5 3
+mpirun -np 16 ./lab1 1 5 3
 echo '<<<'
 
 echo '>>> 2 0 1'
