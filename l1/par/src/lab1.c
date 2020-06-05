342200fb18462f0e8f07a4cb5d76ef1bceaaac16
huerd <2333486+huerd@users.noreply.github.com>
2020-06-04 20:36:20 -0400

    - added function to print memory address

--- l1/par/src/lab1.c
+++ l1/par/src/lab1.c
@@ -47,6 +47,18 @@ void printMatrix(int rows, int cols, int ** matrix) {
     printf("\n");
 }
 
+void printMatrixMemAddress(int rows, int cols, int ** matrix) {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%4p ", (void*)&matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
 void printRuntime(struct timeval tvs, struct timeval tve) {
     long start = tvs.tv_sec * 1000000 + tvs.tv_usec;
     long end = tve.tv_sec * 1000000 + tve.tv_usec;
