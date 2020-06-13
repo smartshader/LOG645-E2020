3364e77bb5e12e931c2c0e02f57ffd40b6677569
huerd <2333486+huerd@users.noreply.github.com>
2020-06-13 15:11:05 -0400

    - modified run file

--- l2/run.sh
+++ l2/run.sh
@@ -10,4 +10,15 @@ echo '>>> 39 2 10 3'
 ./lab2 39 2 10 3
 echo '<<<'
 
+echo '>>> ________ [EVALUATION : Problem #1] ________'
+echo ''
+echo '>>> 53 1 3 600 (Acceleration doit etre plus que 45)'
+./lab2 53 1 3 600
+echo '<<<'
+echo '>>> ________ [EVALUATION : Problem #2] ________'
+echo ''
+echo '>>> 23 2 7 600 (Acceleration doit etre plus que 15)'
+./lab2 23 2 7 600
+echo '<<<'
+
 make clean
